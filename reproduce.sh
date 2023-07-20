3dccfbf13266434ef4879c2dd39e56578998abce
Daniele Baccega <daniele.baccega@unito.it>
2023-07-20 16:13:02 +0200

    Removed docker pull

--- reproduce.sh
+++ reproduce.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker pull danielebaccega/prophet
 docker build -t danielebaccega/reproduce-prophet .
 docker run -it --user 1001:1001 -v $(pwd):/home/docker/prophet-forecasting danielebaccega/reproduce-prophet
