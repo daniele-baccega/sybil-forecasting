af4448b04322cfec44b1b93eec8b5af12bda428c
Daniele Baccega <daniele.baccega@unito.it>
2023-07-20 15:32:33 +0200

    Added docker pull

--- reproduce.sh
+++ reproduce.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker pull danielebaccega/prophet
 docker build -t danielebaccega/reproduce-prophet .
 docker run -it --user 1001:1001 -v $(pwd):/home/docker/prophet-forecasting danielebaccega/reproduce-prophet
