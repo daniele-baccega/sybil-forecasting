a12c24040abcac1a2f3b7cc16eab13612d4f017b
Daniele Baccega <daniele.baccega@unito.it>
2023-07-27 17:49:07 +0200

    Fixed the UID used to run the docker

--- reproduce.sh
+++ reproduce.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t danielebaccega/reproduce-prophet .
-docker run -it --user 1001:1001 -v $(pwd):/home/docker/prophet-forecasting danielebaccega/reproduce-prophet
+docker run -it --user $UID:$UID -v $(pwd):/home/docker/prophet-forecasting danielebaccega/reproduce-prophet
