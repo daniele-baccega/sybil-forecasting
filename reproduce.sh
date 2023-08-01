b8d97ad79147ee5e7a7dbd83d24e8bbd0974c811
Daniele Baccega <daniele.baccega@unito.it>
2023-08-01 16:43:27 +0200

    Add --rm to docker-run

--- reproduce.sh
+++ reproduce.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t danielebaccega/reproduce-prophet .
-docker run -it --user $UID:$UID -v $(pwd):/home/docker/prophet-forecasting danielebaccega/reproduce-prophet
+docker run -it --user $UID:$UID --rm -v $(pwd):/home/docker/prophet-forecasting danielebaccega/reproduce-prophet
