5c3c6694c1e1bcb2e879f55366eea65abe236019
Daniele Baccega <daniele.baccega@unito.it>
2024-03-14 14:23:38 +0100

    Renamed the docker

--- reproduce.sh
+++ reproduce.sh
@@ -5,5 +5,5 @@ then
   git clone git@github.com:daniele-baccega/aggregatesUMD.git
 fi
 
-docker build -t danielebaccega/reproduce-prophet .
-docker run -it --user $UID:$UID --rm -v $(pwd):/home/docker/prophet-forecasting danielebaccega/reproduce-prophet
+docker build -t danielebaccega/reproduce-sybil .
+docker run -it --user $UID:$UID --rm -v $(pwd):/home/docker/sybil-forecasting danielebaccega/reproduce-sybil
