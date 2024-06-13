d03d3178ddf2bc336141f6fb72d27268c70fc38e
Daniele Baccega <daniele.baccega@unito.it>
2024-06-13 12:21:52 +0200

    Predictions on U.S. states

--- reproduce.sh
+++ reproduce.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if [ ! -d aggregatesUMD ];
-then
-  git clone git@github.com:daniele-baccega/aggregatesUMD.git
-fi
+unzip datasets/USA_2024-06-13.zip -d datasets
+unzip datasets/USA_variants.zip -d datasets
 
 docker build -t danielebaccega/reproduce-sybil .
 docker run -it --user $UID:$UID --rm -v $(pwd):/home/docker/sybil-forecasting danielebaccega/reproduce-sybil
